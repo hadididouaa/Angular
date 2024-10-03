88ce92d57a545a6d5c3cbf6749f3263708ce7444
Douaa Ghofrane Hadidi <douaaghofrane.hadidi@esprit.tn>
2024-10-03 08:23:55 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListCategoriesComponent } from './list-categories/list-categories.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //liste des composants attaches 
+    AppComponent,
+    HeaderComponent,
+    ListCategoriesComponent,
+    FooterComponent,
+    HomeComponent
+    
   ],
-  imports: [
-    BrowserModule
+  imports: [//liste des modules a utiliser 
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [],//services a utiliser 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
